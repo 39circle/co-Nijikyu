0c8e354006ecb73e193d3fa563ec530df6d23716
nikki9750 <76221172+tam1192@users.noreply.github.com>
2026-07-10 00:28:42 +0900

    homeをtopに名前変更

--- app/routes/Top.tsx
+++ app/routes/Top.tsx
@@ -1,6 +1,6 @@
 // app/routes/home.tsx (または src/routes/home.tsx)
 import type { Route } from "./+types/Home";
-import {Home} from "../pages/home/Home"
+import {Top} from "../pages/top/Top"
 
 
 // ページのメタデータ（SEOやタブのタイトル）を定義
@@ -14,5 +14,5 @@ export function meta({}: Route.MetaArgs) {
 // React Router v7 では、Loaderなどで取得したデータをこの args から型安全に受け取れます
 export default function Page({ loaderData }: Route.ComponentProps) {
   // 実際の見た目は外部の Page コンポーネントに任せる
-  return <Home/>
+  return <Top/>
 }
